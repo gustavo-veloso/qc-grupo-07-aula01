dd3b30c6b5117f125c1ef1f8b41e153420830ab8
gustavo-veloso <gustavo.veloso@gmail.com>
2026-06-07 11:34:34 -0400

    atualizacoes

--- scripts/exercicio-3-1-destroy-terraform.sh
+++ scripts/exercicio-3-1-destroy-terraform.sh
@@ -2,7 +2,7 @@
 
 echo "Iniciando execução do script de destruição do Terraform..."
 
-cd ~/aie-cloud/aulas/01-fundamentos-iac/lab/terraform
+cd ~/qc-grupo-07-aula01/terraform
 
 # Destruir TUDO ao final (regra de ouro — custo zero)
 terraform destroy -var="meu_ip=$MEU_IP"
